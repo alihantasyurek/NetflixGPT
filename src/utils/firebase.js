aa215616c75b8c7424fdd99d59d90c61be2d0bc1
Via <alihantasyurek@gmail.com>
2026-01-05 11:48:06 +0300

    firebase update

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,19 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API,
-  authDomain: "netflixgpt-b4f68.firebaseapp.com",
-  projectId: "netflixgpt-b4f68",
-  storageBucket: "netflixgpt-b4f68.firebasestorage.app",
-  messagingSenderId: "403468345026",
-  appId: "1:403468345026:web:0b0a4e6edd7cc1a387e55b",
-  measurementId: "G-BQ95F8JMMT",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "netmini-f2f72.firebaseapp.com",
+  projectId: "netmini-f2f72",
+  storageBucket: "netmini-f2f72.firebasestorage.app",
+  messagingSenderId: "885289872638",
+  appId: "1:885289872638:web:59252802fba5e4508189af",
+  measurementId: "G-WDJ50BMJCE",
 };
 
 // Initialize Firebase
